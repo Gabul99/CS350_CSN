25e591696af18968e8d9776ee104a4996478894e
Gabul99 <hclee99@kaist.ac.kr>
2023-05-19 00:07:52 +0900

    불필요한 코드 삭제

--- App.tsx
+++ App.tsx
@@ -11,16 +11,12 @@ import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View
 } from "react-native";
 
 import {
   Colors,
 } from "react-native/Libraries/NewAppScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MainLayout from "./src/screens/MainLayout";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -41,24 +37,5 @@ function App(): JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600"
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400"
-  },
-  highlight: {
-    fontWeight: "700"
-  }
-});
 
 export default App;
