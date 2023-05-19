df618d5959d41fb95686516d0a04d384085ccad6
Gabul99 <hclee99@kaist.ac.kr>
2023-05-19 21:05:47 +0900

    TopBar에 아이콘 추가

--- src/components/subscribed/SubscribeTopBar.tsx
+++ src/components/subscribed/SubscribeTopBar.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { WithLocalSvg } from "react-native-svg";
+import { View } from "react-native";
 
 const Container = styled.View`
   width: 100%;
@@ -11,16 +13,28 @@ const Container = styled.View`
   padding: 0 28px;
 `;
 
+const Icons = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+`;
+
 const Title = styled.Text`
   font-family: 'Helvetica Neue';
   font-size: 32px;
   font-weight: 700;
+  margin-right: auto;
 `;
 
 const SubscribeTopBar = () => {
   return (
     <Container>
       <Title>Subscribed</Title>
+      <Icons>
+        <WithLocalSvg asset={require("../../assets/icons/ic_explore.svg")} width={32} height={32} />
+        <WithLocalSvg asset={require("../../assets/icons/ic_search.svg")} width={32} height={32} />
+      </Icons>
     </Container>
   );
 };
