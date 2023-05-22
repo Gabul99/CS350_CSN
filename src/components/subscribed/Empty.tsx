8895e5a073b907a1c07122105a0a50cc0a967172
Gabul99 <hclee99@kaist.ac.kr>
2023-05-23 00:10:58 +0900

    empty에 arrow 까지 추가

--- src/components/subscribed/Empty.tsx
+++ src/components/subscribed/Empty.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components/native";
 import CSText, { FontType } from "../core/CSText";
 import { Colors } from "../../style/Colors";
+import { WithLocalSvg } from "react-native-svg";
 
 const Container = styled.View`
   width: 100%;
@@ -27,6 +28,31 @@ const Ellipsis = styled.View`
   border-radius: 6px;
 `;
 
+const FloatWrapper = styled.View`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+const FloatIcons = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  gap: 16px;
+  padding-right: 6px;
+`;
+
+const FloatText = styled.View`
+  padding: 6px 12px;
+  background-color: ${Colors.WHITE100};
+  border-radius: 24px;
+  overflow: hidden;
+  margin-top: 10px;
+`;
+
 const Empty = () => {
   return (
     <Container>
@@ -37,6 +63,17 @@ const Empty = () => {
         </CSText>
         <Ellipsis />
       </Content>
+      <FloatWrapper>
+        <FloatIcons>
+          <WithLocalSvg asset={require("../../assets/icons/ic_arrow_up.svg")} width={28} height={28} color={Colors.GREEN_DARK} />
+          <WithLocalSvg asset={require("../../assets/icons/ic_arrow_up.svg")} width={28} height={28} color={Colors.GREEN_DARK} />
+        </FloatIcons>
+        <FloatText>
+          <CSText fontType={FontType.REGULAR} fontSize={18} color={Colors.GREEN_DARK}>
+            Try navigate / search clubs
+          </CSText>
+        </FloatText>
+      </FloatWrapper>
     </Container>
   );
 };
