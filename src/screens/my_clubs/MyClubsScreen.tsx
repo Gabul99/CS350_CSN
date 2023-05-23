6805c0196dbb4a54b616a864b56c6afd0b1ac342
Moyeon <dbqld770@gmail.com>
2023-05-24 05:31:35 +0900

    MyClubScreen에 임시로 ClubDetail 삽입

--- src/screens/my_clubs/MyClubsScreen.tsx
+++ src/screens/my_clubs/MyClubsScreen.tsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Colors } from "../../style/Colors";
+import ClubDetailScreen from "./ClubDetailScreen";
 
+const Container = styled.View`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${Colors.GREEN_BACKGROUND};
+`;
 interface Props {
   navigation: any;
   rootNavigation: any;
@@ -8,9 +17,12 @@ interface Props {
 
 const MyClubsScreen = ({ navigation, rootNavigation }: Props) => {
   return (
-    <Text>
-      My clubs
-    </Text>
+    <>
+      <ClubDetailScreen></ClubDetailScreen>
+      <Text>
+        My clubs
+      </Text>
+    </>
   );
 };
 
