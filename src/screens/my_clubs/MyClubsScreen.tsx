b7cb2b8c7527e6f709c7c7f04943acb8ea743f69
Gabul99 <hclee99@kaist.ac.kr>
2023-05-26 01:16:57 +0900

    FloatingCreatePost 버튼 추가

--- src/screens/my_clubs/MyClubsScreen.tsx
+++ src/screens/my_clubs/MyClubsScreen.tsx
@@ -4,6 +4,7 @@ import { Colors } from "../../style/Colors";
 import CSText, { FontType } from "../../components/core/CSText";
 import ClubSelectBar from "../../components/my_clubs/ClubSelectBar";
 import FeedPost from "../../components/core/FeedPost";
+import { WithLocalSvg } from "react-native-svg";
 
 const Container = styled.View`
   height: 100%;
@@ -28,6 +29,24 @@ const ScrollArea = styled.ScrollView`
   flex-direction: column;
 `;
 
+const ContentArea = styled.View`
+  position: relative;
+  padding-bottom: 44px;
+`;
+
+const FloatingCreatePost = styled.TouchableOpacity`
+  position: absolute;
+  width: 60px;
+  height: 60px;
+  border-radius: 30px;
+  bottom: 52px;
+  right: 8px;
+  background-color: ${Colors.GREEN_DEEP};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 interface Props {
   navigation: any;
   rootNavigation: any;
@@ -41,12 +60,17 @@ const MyClubsScreen = ({ navigation, rootNavigation }: Props) => {
           My Clubs
         </CSText>
       </TopBarContainer>
-      <ScrollArea contentContainerStyle={{ rowGap: 6 }}>
-        <ClubSelectBar />
-        <FeedPost />
-        <FeedPost />
-        <FeedPost />
-      </ScrollArea>
+      <ContentArea>
+        <ScrollArea contentContainerStyle={{ rowGap: 6 }}>
+          <ClubSelectBar />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+        </ScrollArea>
+        <FloatingCreatePost>
+          <WithLocalSvg asset={require("../../assets/icons/ic_stylus.svg")} width={32} height={32} />
+        </FloatingCreatePost>
+      </ContentArea>
     </Container>
   );
 };
