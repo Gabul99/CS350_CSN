354f35e14e396c3c85b2991cec944aa87765b842
Gabul99 <hclee99@kaist.ac.kr>
2023-05-26 00:30:21 +0900

    ClubSelectBar 추가

---
+++ src/components/my_clubs/ClubSelectBar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components/native";
+import { ScrollView, Text } from "react-native";
+import { Colors } from "../../style/Colors";
+
+const ScrollContainer = styled.View`
+  width: 100%;
+  height: 78px;
+  background-color: ${Colors.WHITE100};
+  padding: 12px 0;
+`;
+
+const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  padding: 0 12px;
+  gap: 8px;
+  align-items: center;
+`;
+
+const ClubSelectBar = () => {
+  return (
+    <ScrollContainer>
+      <ScrollView horizontal>
+        <Container>
+          <Text>hi</Text>
+          <Text>hi</Text>
+        </Container>
+      </ScrollView>
+    </ScrollContainer>
+  );
+};
+
+export default ClubSelectBar;
--- src/screens/my_clubs/MyClubsScreen.tsx
+++ src/screens/my_clubs/MyClubsScreen.tsx
@@ -1,5 +1,25 @@
 import React from "react";
-import { Text } from "react-native";
+import styled from "styled-components/native";
+import { Colors } from "../../style/Colors";
+import CSText, { FontType } from "../../components/core/CSText";
+import ClubSelectBar from "../../components/my_clubs/ClubSelectBar";
+
+const Container = styled.View`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: ${Colors.GREEN_BACKGROUND};
+`;
+
+const TopBarContainer = styled.View`
+  width: 100%;
+  height: 44px;
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  align-items: center;
+  padding: 0 16px;
+`;
 
 interface Props {
   navigation: any;
@@ -8,9 +28,14 @@ interface Props {
 
 const MyClubsScreen = ({ navigation, rootNavigation }: Props) => {
   return (
-    <Text>
-      My clubs
-    </Text>
+    <Container>
+      <TopBarContainer>
+        <CSText fontType={FontType.BOLD} fontSize={24}>
+          My Clubs
+        </CSText>
+      </TopBarContainer>
+      <ClubSelectBar />
+    </Container>
   );
 };
 
